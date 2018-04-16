c76e3c7775efa5f9c3493b5cde632340577cf129
Erik Clevenger <eclevenger@cnm.edu>
2018-04-15 20:40:06 -0600

    initial index

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Pro Dev Doc</title>
+	</head>
+	<body>
+		<h1>Erik's Pro Dev Doc</h1>
+		<ul>
+			<li>updated 4/15/18</li>
+			<li>Career Buddy: James Ticer</li>
+		</ul>
+		<h2>Personal Links</h2>
+		<a href="https://github.com/erikClevenger">GitHUb</a>
+		<a href="https://www.linkedin.com/in/modwrk/">LinkedIn</a>
+		<h2>Deep Dive Homework</h2>
+		<a href="https://bootcamp-coders.cnm.edu/~eclevenger/data-design/index.php">Data Design</a>
+		<div class="section">
+			<div  class="split quantity">
+				<h2>Quantitative Accomplishments</h2>
+					<ul>
+						<li>I studied 5 different artistic mediums at advanced levels in college.
+							<ul>
+								<li>Painting</li>
+								<li>Printmaking</li>
+								<li>Drawing</li>
+								<li>Sculpture</li>
+								<li>Ceramics</li>
+							</ul>
+						</li>
+						<li>I designed the interfaces for two existing mobile applications.</li>
+						<li>I've been a musician in some capacity for over 20 years.</li>
+					</ul>
+			</div>
+			<div class="split quality">
+				<h2>Qualitative</h2>
+				<p>I am an intuitive and creative problem solver by nature.</p>
+				<p>My natural inclination is to look at problems from several angles.</p>
+				<p>I am inherently altruistic.</p>
+			</div>
+		</div>
+		<div class="section">
+			<div class="split golden">
+				<h2>Golden Circle</h2>
+				<p>Why: I want to support the local community</p>
+				<p>How: Mobile application development</p>
+				<p>What: Build a platform for community driven organizations to connect to sources of funding</p>
+			</div>
+			<div class="split elevator">
+				<h2>Elevator Pitch</h2>
+				<p>My name is erik. I’m finding creative solutions for funding community driven organizations and nonprofits.
+					<br>To do that I’m building a platform that connects potential donors to organizations with the same goals.
+					<br>Who do you want to support?</p>
+			</div>
+		</div>
+		<div class="section">
+			<article class="reflection weekZero">
+				<h2>Week Zero Reflection</h2>
+				<p>
+				</p>
+			</article>
+		</div>
+	</body>
+</html>
