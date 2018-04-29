4f9ad23a75a7154856c74fe9b65e99099cbdab20
Erik Clevenger <eclevenger@cnm.edu>
2018-04-29 12:33:46 -0600

    add updated resume

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<h2>Personal Links</h2>
 		<a href="https://github.com/erikClevenger">GitHUb</a>
 		<a href="https://www.linkedin.com/in/modwrk/">LinkedIn</a>
-		<a href="resume-erikclevenger.pdf">Resume</a>
+		<a href="resume_ErikClevenger.pdf">Resume</a>
 		<h2>Deep Dive Homework</h2>
 		<a href="https://bootcamp-coders.cnm.edu/~eclevenger/data-design/index.php">Data Design</a>
 		<section>
