a92477de796c74b1a35122523b3bc88ebaa225cb
Erik Clevenger <eclevenger@cnm.edu>
2018-05-10 10:48:37 -0600

    add cover letter

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		<a href="https://github.com/erikClevenger">GitHub</a>
 		<a href="https://www.linkedin.com/in/modwrk/">LinkedIn</a>
 		<a href="resume-ErikClevenger.pdf">Resume</a>
+		<a href="erik-clevenger-cover.pdf">Meow Wolf cover letter.</a>
 		<h2>Deep Dive Homework</h2>
 		<a href="https://bootcamp-coders.cnm.edu/~eclevenger/data-design/index.php">Data Design</a>
 		<section>
